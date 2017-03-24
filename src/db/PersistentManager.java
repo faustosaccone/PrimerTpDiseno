42f6b342392912d6df6ee79237e997a8280d0100
faustosaccone <fausto_2-8@hotmail.com>
2017-03-24 20:27:38 -0300

    Primeros 3 pasos

---
+++ src/db/PersistentManager.java
@@ -0,0 +1,13 @@
+package db;
+import java.util.ArrayList;
+import utn.frd.bean.Persona;
+
+
+public class PersistentManager {
+	private static ArrayList<Persona> personas = new ArrayList<Persona>();
+	
+	public static ArrayList<Persona> getInstance(){
+		return personas;
+	}
+
+}
---
+++ src/utn/frd/bean/Persona.java
@@ -0,0 +1,21 @@
+package utn.frd.bean;
+
+public class Persona {
+	private long id;
+	private String name;
+	private int age;
+	private String gender;
+	
+	public Persona(int identificador, String nombre, int edad, String genero){
+		id = identificador;
+		name = nombre;
+		age = edad;
+		gender = genero;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
+
